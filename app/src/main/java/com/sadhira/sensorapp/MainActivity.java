e1122f21fef505174ec5d67b1dcd056ccebc0682
Sadhira <sadhira.rw@gmail.com>
2016-06-03 18:23:06 +0100

    Put bluetooth functions into content_main instead of activity_main (swapped code between the files).
    Also made sure MainActivity now obtained its layout from content_main instead.

--- app/src/main/java/com/sadhira/sensorapp/MainActivity.java
+++ app/src/main/java/com/sadhira/sensorapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends Activity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
 
         b1 = (Button) findViewById(R.id.button);
         b2=(Button)findViewById(R.id.button2);
